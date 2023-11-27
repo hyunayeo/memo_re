bd99a03750c52e3b3d70ee63ed53e5eaed6833ff
Lyrics109 <rily1108@gmail.com>
2023-11-27 10:04:13 +0900

    유저 파일 삭제

--- src/main/java/kitri/dev6/memore/User/ResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package kitri.dev6.memore.User;
-
-public class ResponseDTO {
-}
--- src/main/java/kitri/dev6/memore/User/UserService.java
+++
@@ -1,8 +0,0 @@
-package kitri.dev6.memore.User;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-}
