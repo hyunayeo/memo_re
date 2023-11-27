f89b65ab51c6174615ab145fada14bf9c1cf43f5
Lyrics109 <rily1108@gmail.com>
2023-11-27 09:57:58 +0900

    드디어 인서트 되다..!

--- src/main/java/kitri/dev6/memore/controller/ArticleController.java
+++ src/main/java/kitri/dev6/memore/controller/ArticleController.java
@@ -3,6 +3,7 @@ package kitri.dev6.memore.controller;
 import kitri.dev6.memore.domain.Article;
 import kitri.dev6.memore.dto.ArticleRequest;
 import kitri.dev6.memore.repository.ArticleMapper;
+import kitri.dev6.memore.service.ArticleService;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,57 +16,35 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleController {
 
-    @Autowired
-    private ArticleMapper articleMapper;
+
+    private final ArticleService articleService;
 
 
     @PostMapping("/list")
     public List<Article> list(){
-        return articleMapper.findAll();
+        return articleService.list();
     }
 
 
     @PostMapping()
-    public Long create(@RequestBody ArticleRequest articleRequest) {
-        if (articleRequest != null) {
-            Article article = new Article();
-            article.setTitle(articleRequest.getTitle());
-            article.setContent(articleRequest.getContent());
-            article.setDone(articleRequest.isDone());
-            article.setRatingScore(articleRequest.getRatingScore());
-            article.setStartDate(articleRequest.getStartDate());
-            article.setEndDate(articleRequest.getEndDate());
-            article.setHide(articleRequest.isHide());
-            return articleMapper.insert(article);
-        }
-        return null;
+    public Long create(@RequestBody Article article) {
+        return articleService.create(article);
     }
 
 
     @GetMapping("/{id}")
     public Article findById(@PathVariable String id){
-        return articleMapper.findById(Long.parseLong(id));
+        return articleService.findById(id);
     }
 
     @PutMapping("/{id}")
     public Long update(@PathVariable String id, @RequestBody ArticleRequest articleRequest){
-        Article article = articleMapper.findById(Long.parseLong(id));
-        System.out.println(article.getId());
-        if (article == null) return null;
-        article.setTitle(articleRequest.getTitle());
-        article.setContent(articleRequest.getContent());
-        article.setDone(articleRequest.isDone());
-        article.setRatingScore(articleRequest.getRatingScore());
-        article.setStartDate(articleRequest.getStartDate());
-        article.setEndDate(articleRequest.getEndDate());
-        article.setHide(articleRequest.isHide());
-
-        return articleMapper.update(article);
+       return articleService.update(id, articleRequest);
     }
 
     @GetMapping("/delete/{id}")
     public Long delete(@PathVariable String id){
-        return articleMapper.delete(Long.parseLong(id));
+        return articleService.delete(id);
     }
 
 
--- src/main/java/kitri/dev6/memore/service/ArticleService.java
+++ src/main/java/kitri/dev6/memore/service/ArticleService.java
@@ -1,30 +1,62 @@
 package kitri.dev6.memore.service;
 
 import kitri.dev6.memore.domain.Article;
+import kitri.dev6.memore.dto.ArticleRequest;
 import kitri.dev6.memore.repository.ArticleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ArticleService {
+
+    @Autowired
     private ArticleMapper articleMapper;
 
-    public List<Article> articleList(){
+    public List<Article> list(){
         return articleMapper.findAll();
     }
 
-    public Article findById(Long articleId){
-        return articleMapper.findById(articleId);
+    public Article findById(String id){
+        return articleMapper.findById(Long.parseLong(id));
     }
 
-    public Long update(Long id){
-        Article article = articleMapper.findById(id);
+    public Long create(Article article){
+        if (article != null) {
+            article.setMemberId(article.getMemberId());
+            article.setBookId(article.getBookId());
+            article.setTitle(article.getTitle());
+            article.setContent(article.getContent());
+            article.setDone(article.isDone());
+            article.setRatingScore(article.getRatingScore());
+            article.setStartDate(article.getStartDate());
+            article.setEndDate(article.getEndDate());
+            article.setHide(article.isHide());
+            articleMapper.insert(article);
+        }
+        return null;
+    }
+
+    public Long update(String id, ArticleRequest articleRequest){
+//        Article article = articleMapper.findById(id);
+//        if (article == null) return null;
+//        return articleMapper.update(article);
+        Article article = articleMapper.findById(Long.parseLong(id));
+        System.out.println(article.getId());
         if (article == null) return null;
+        article.setTitle(articleRequest.getTitle());
+        article.setContent(articleRequest.getContent());
+        article.setDone(articleRequest.isDone());
+        article.setRatingScore(articleRequest.getRatingScore());
+        article.setStartDate(articleRequest.getStartDate());
+        article.setEndDate(articleRequest.getEndDate());
+        article.setHide(articleRequest.isHide());
+
         return articleMapper.update(article);
     }
 
-    public Long delete(Long articleId){
-        return articleMapper.delete(articleId);
+    public Long delete(String id){
+        return articleMapper.delete(Long.parseLong(id));
     }
 }
