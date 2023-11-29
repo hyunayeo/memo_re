4d303569a01505e2c810cb769d263e3a726493f3
captain-yun <jslovejs182@gmail.com>
2023-11-29 16:45:33 +0900

    #16 - 정렬 기준을 위한 필드 추가 sortFieldType : 어떤 필드인지 sortAs : DESC || ASC

--- src/main/java/kitri/dev6/memore/dto/common/SearchDto.java
+++ src/main/java/kitri/dev6/memore/dto/common/SearchDto.java
@@ -10,6 +10,8 @@ public class SearchDto {
     private int page;             // 현재 페이지 번호
     private int recordSize;       // 페이지당 출력할 데이터 개수
     private int pageSize;         // 화면 하단에 출력할 페이지 사이즈
+    private String sortFieldType;     // 정렬 기준
+    private String sortAs;    // 정렬 순서
     private String keyword;       // 검색 키워드
     private String searchType;    // 검색 유형 (제목, 아이디, 날짜...)
     private String domainType;
@@ -19,10 +21,11 @@ public class SearchDto {
         this.page = 1;
         this.recordSize = 10;
         this.pageSize = 10;
+        this.sortFieldType = Type.ID;
+        this.sortAs = Type.DESC;
     }
 
     public int getOffset() {
         return (page - 1) * recordSize;
     }
-
 }
