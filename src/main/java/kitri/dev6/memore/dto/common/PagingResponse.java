6a0a35b66b8c0e65e4869db9241ad60bc81fdf47
captain-yun <jslovejs182@gmail.com>
2023-11-30 18:04:14 +0900

    #21 - PagingResponse에 _links 필드 추가 - hateaos 를 충족하기 위해

--- src/main/java/kitri/dev6/memore/dto/common/PagingResponse.java
+++ src/main/java/kitri/dev6/memore/dto/common/PagingResponse.java
@@ -1,16 +1,19 @@
 package kitri.dev6.memore.dto.common;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Link;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 public class PagingResponse<T> {
 
     private List<T> list = new ArrayList<>();
     private Pagination pagination;
-
+    private Link _links;
     public PagingResponse(List<T> list, Pagination pagination) {
         if (list != null) this.list.addAll(list);
         this.pagination = pagination;
