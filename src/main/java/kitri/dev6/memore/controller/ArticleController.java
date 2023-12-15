0090ab27f3821336e92493c01c76ab49b88def4f
hyunayeo <48472568+hyunayeo@users.noreply.github.com>
2023-12-15 09:38:07 +0900

    게시글 등록과 수정시 memberId 가 본인인지 확인하고 처리 (#35)

--- src/main/java/kitri/dev6/memore/controller/ArticleController.java
+++ src/main/java/kitri/dev6/memore/controller/ArticleController.java
@@ -1,23 +1,16 @@
 package kitri.dev6.memore.controller;
 
+import kitri.dev6.memore.configuration.auth.LoginUser;
+import kitri.dev6.memore.configuration.auth.dto.SessionUser;
 import kitri.dev6.memore.domain.Article;
 import kitri.dev6.memore.dto.common.PagingResponse;
 import kitri.dev6.memore.dto.common.SearchDto;
 import kitri.dev6.memore.dto.request.ArticleRequestDto;
-import kitri.dev6.memore.dto.response.ArticleResponseDto;
-import kitri.dev6.memore.dto.response.MemberResponseDto;
 import kitri.dev6.memore.service.ArticleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/articles")
@@ -43,13 +36,19 @@ public class ArticleController {
         return new ResponseEntity<>(article, HttpStatus.OK);
     }
     @PostMapping
-    public Long insert(@RequestBody ArticleRequestDto articleRequestDto) {
-        return articleService.insert(articleRequestDto);
+    public Long insert(@LoginUser SessionUser user, @RequestBody ArticleRequestDto articleRequestDto) {
+        if (user.getId().equals(articleRequestDto.getMemberId())){
+            return articleService.insert(articleRequestDto);
+        }
+        throw new IllegalArgumentException("부적절한 요청");
     }
     @PutMapping("/{id}")
-    public Long update(@PathVariable Long id, @RequestBody ArticleRequestDto articleRequestDto) {
-        articleService.update(id, articleRequestDto);
-        return id;
+    public Long update(@LoginUser SessionUser user, @PathVariable Long id, @RequestBody ArticleRequestDto articleRequestDto) {
+        if (user.getId().equals(articleRequestDto.getMemberId())){
+            articleService.update(id, articleRequestDto);
+            return id;
+        }
+        throw new IllegalArgumentException("부적절한 요청");
     }
     @DeleteMapping("/{id}")
     public Long delete(@PathVariable Long id) {
