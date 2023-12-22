0b912909c1795ea07d8cb8414b53488588dcbd37
kitri.ha <picnic935@gmail.com>
2023-12-22 13:13:07 +0900

    swagger description 추가

--- src/main/java/kitri/dev6/memore/controller/ArticleController.java
+++ src/main/java/kitri/dev6/memore/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package kitri.dev6.memore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import kitri.dev6.memore.configuration.auth.LoginUser;
 import kitri.dev6.memore.configuration.auth.dto.SessionUser;
 import kitri.dev6.memore.domain.Article;
@@ -19,6 +20,7 @@ public class ArticleController {
 
     private final ArticleService articleService;
     @GetMapping
+    @Operation(summary = "게시글 검색", description = "검색어를 이용하여 게시글을 검색합니다.")
     public ResponseEntity<PagingResponse<Article>> findAll(@ModelAttribute("params") SearchDto params) {
 
         PagingResponse<Article> articles = articleService.findAll(params);
@@ -28,6 +30,7 @@ public class ArticleController {
         return new ResponseEntity<>(articles, HttpStatus.OK);
     }
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 단일 조회", description = "게시글 아이디를 이용하여 게시글을 조회합니다.")
     public ResponseEntity<Article> findById(@PathVariable Long id) {
         Article article = articleService.findById(id);
         if (article == null) {
@@ -36,6 +39,7 @@ public class ArticleController {
         return new ResponseEntity<>(article, HttpStatus.OK);
     }
     @PostMapping
+    @Operation(summary = "게시글 등록", description = "제목과 내용, 도서정보 등을 입력하여 게시글을 등록합니다.")
     public Long insert(@LoginUser SessionUser user, @RequestBody ArticleRequestDto articleRequestDto) {
         if (user.getId().equals(articleRequestDto.getMemberId())){
             return articleService.insert(articleRequestDto);
@@ -43,6 +47,7 @@ public class ArticleController {
         throw new IllegalArgumentException("부적절한 요청");
     }
     @PutMapping("/{id}")
+    @Operation(summary = "게시글 수정", description = "게시글 아이디를 이용하여 게시글을 수정합니다.")
     public Long update(@LoginUser SessionUser user, @PathVariable Long id, @RequestBody ArticleRequestDto articleRequestDto) {
         if (user.getId().equals(articleRequestDto.getMemberId())){
             articleService.update(id, articleRequestDto);
@@ -51,6 +56,7 @@ public class ArticleController {
         throw new IllegalArgumentException("부적절한 요청");
     }
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제", description = "게시글 아이디를 이용하여 게시글을 삭제합니다.")
     public Long delete(@LoginUser SessionUser user, @PathVariable Long id) {
         articleService.delete(user.getId(),id);
         return id;
