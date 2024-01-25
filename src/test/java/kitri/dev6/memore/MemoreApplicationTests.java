2b25c228f083864e65b022741ab722fed7a1d11e
hyuna.home <48472568+hyuna.home@users.noreply.github.com>
2024-01-26 00:26:55 +0900

    update test

--- src/test/java/kitri/dev6/memore/MemoreApplicationTests.java
+++ src/test/java/kitri/dev6/memore/MemoreApplicationTests.java
@@ -3,7 +3,6 @@ package kitri.dev6.memore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MemoreApplicationTests {
     @Test
     void contextLoads() {
